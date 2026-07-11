2de77313921f7f7a723b7d7c564e27cdc7a82430
Natansh14687 <natansh522@gmail.com>
2026-07-11 18:29:45 +0530

    Login route - login api - axios - cors()

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Body from "./Body";
 import ErrorElement from "./ErrorElement";
-
+import Login from "./Login";
 
 function App() {
   return (
     <>
       <BrowserRouter basename="/">
-      <Routes>
-        <Route path="/" element={<Body />}>
-          <Route path="/login" element={<div>Login page</div>}/>
-          <Route path="/test" element={<div>Test page</div>}/>
-          <Route path="*" element={<ErrorElement />}/>
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/test" element={<div>Test page</div>} />
+            <Route path="*" element={<ErrorElement />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ src/Login.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("natansh@khurana.com");
+  const [password, setPassword] = useState("Natansh@123");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/login",
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        },
+      );
+    } catch (err) {
+      console.log("Error : " + err.message);
+    }
+  };
+  return (
+    <div className="flex justify-center my-10">
+      <div className="card card-border bg-base-100 w-96">
+        <div className="card-body">
+          <h2 className="card-title flex justify-center">Login</h2>
+          <div>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Email : </legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Type here"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </fieldset>
+            <fieldset className="fieldset">
+              <legend className="fieldset-legend">Password : </legend>
+              <input
+                type="text"
+                className="input"
+                placeholder="Type here"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </fieldset>
+          </div>
+          <div className="card-actions justify-center mt-4">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
