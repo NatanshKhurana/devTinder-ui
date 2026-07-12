4a293ccea026ad527a25185b6ebc9cf4e885df04
Natansh14687 <natansh522@gmail.com>
2026-07-12 20:09:19 +0530

    get connections route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import { Provider } from "react-redux";
 import appStore from "../store/appStore";
 import Feed from "./Feed";
 import Profile from "./Profile";
+import Connections from "./Connections";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Body />}>
             <Route path="/login" element={<Login />} />
             <Route path="/profile" element={<Profile />}/>
+            <Route path="/connections" element={<Connections />}/>
             <Route path="/" element={<Feed />} />
             <Route path="*" element={<ErrorElement />} />
           </Route>
---
+++ src/components/Connections.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addConnections } from "../store/connectionSlice";
+import { useSelector } from "react-redux";
+
+const Connections = () => {
+  const connections = useSelector((store) => store.connection);
+  const dispatch = useDispatch();
+  const getConnections = async () => {
+    try {
+      const res = await axios.get("http://localhost:7777/user/connections", {
+        withCredentials: true,
+      });
+      //   console.log(res.data);
+      dispatch(addConnections(res.data));
+    } catch (err) {
+      console.dir(err);
+    }
+  };
+
+  useEffect(() => {
+    getConnections();
+  }, []);
+
+  if (!connections) return "";
+  if (connections.length === 0)
+    return <div className="text-lg font-bold text-center my-40">No connections, Send request to get connections</div>;
+
+  return (
+    <div className="">
+      <div className="text-3xl font-bold text-white flex justify-center mt-8">
+        Connections
+      </div>
+      {connections.map((connection) => {
+        return (
+          <div className="card card-side bg-base-200 h-40 shadow-sm mx-auto w-1/2 my-4" key={connection._id}>
+            <figure className="p-4">
+              <img
+                src={connection.photoUrl}
+                alt="Movie"
+                className="rounded-full"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{connection.firstName} {connection.lastName}</h2>
+              {connection.age && connection.gender && <p>{connection.age} - {connection.gender}</p>}
+              {connection.about && <p>{connection.about}</p>}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Connections;
