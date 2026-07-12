cea85e5b690d4c244496dcec823897eda95d5a20
Natansh14687 <natansh522@gmail.com>
2026-07-12 11:58:27 +0530

    user already logged in or not - yes => feed route => no => login route

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import Login from "./Login";
 import { Provider } from "react-redux";
 import appStore from "../store/appStore";
 import Feed from "./Feed";
+import Profile from "./Profile";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Body />}>
             <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />}/>
             <Route path="/" element={<Feed />} />
             <Route path="*" element={<ErrorElement />} />
           </Route>
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import { Outlet } from 'react-router';
-
+import { Outlet } from "react-router";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../store/userSlice";
+import { useNavigate } from "react-router";
+import { useSelector } from "react-redux";
 
 const Body = () => {
+  const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+  const fetchUser = async () => {
+    try {
+      if (user) return;
+      const res = await axios.get("http://localhost:7777/profile/view", {
+        withCredentials: true,
+      });
+      // console.log(res);
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.status === 401) {
+        navigate("/login");
+      }
+      console.log("Error : " + err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <Outlet />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
